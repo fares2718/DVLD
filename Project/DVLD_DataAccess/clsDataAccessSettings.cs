bbf3086c96a69d7015dbda84c384dfe3b16773c5
fares2718 <faresobaid2715@gmail.com>
2025-07-13 00:09:51 +0300

    V1.0 of DVLD Project

---
+++ Project/DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=DESKTOP-O1ENIR6;Database=DVLD;User Id=sa;Password=sa123456;";
+
+
+    }
+}
